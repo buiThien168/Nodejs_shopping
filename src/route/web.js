e739b2f779ccd09cd7ade67fc7586f27fd983325
buithien <thien7278@gmail.com>
2023-02-21 22:40:31 +0700

    REST APIS login Model Login

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 let route =  express.Router();
 let initWebRoutes = (app)=>{
     route.get('/',homeController.getHomePage);
@@ -10,6 +11,7 @@ let initWebRoutes = (app)=>{
     route.get('/edit-crud',homeController.getEditCRUD);
     route.post('/put-crud',homeController.putCRUD);
     route.get('/delete-crud',homeController.deleteCRUD);
+    route.post('/api/login',userController.handleLogin);
     return app.use("/",route);
 }
 module.exports = initWebRoutes; 
---
+++ src/services/userService.js
@@ -0,0 +1,69 @@
+import db from "../models/index";
+import bcrypt, { hash } from "bcryptjs";
+
+let handleUserLogin = (email, password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userData = {};
+      let isExist = await checkUserEmail(email);
+      if (isExist) {
+        let user = await db.User.findOne({
+          attributes: ["email", "roleID", "password"],
+          where: { email: email },
+          raw:true
+        });
+        if (user) {
+          let check = await bcrypt.compareSync(password, user.password);
+          if (check) {
+            userData.errCode = 0;
+            userData.errMessage = "ok";
+            delete user.password;
+            userData.user = user;
+          } else {
+            userData.errCode = 3;
+            userData.errMessage = "Wrong password";
+          }
+        } else {
+          userData.errCode = 2;
+          userData.errMessage = `User's not found`;
+        }
+      } else {
+        userData.errCode = 1;
+        userData.errMessage = `Your's Email isn't exist in your system. PLZ try other email`;
+      }
+      resolve(userData);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+let checkUserEmail = (userEmail) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { email: userEmail },
+      });
+      if (user) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+// let compareUserPassword=()=>{
+//     return Promise((resolve,reject)=>{
+//         try {
+
+//         } catch (error) {
+//             reject(error)
+//         }
+//     })
+// }
+// giair max password
+module.exports = {
+  handleUserLogin: handleUserLogin,
+};
