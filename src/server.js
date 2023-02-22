31853bc87cbd3964568543be281b948a300544f3
buithien <thien7278@gmail.com>
2023-02-22 23:00:14 +0700

    Add_Lib_cors_Login

--- src/server.js
+++ src/server.js
@@ -2,20 +2,29 @@ import express from "express";
 import bodyParser, { urlencoded } from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoute from "./route/web";
-import connectDB from './config/connectDB'; 
-require('dotenv').config();
+import connectDB from "./config/connectDB";
+import cors from "cors";
+
+require("dotenv").config();
 let app = express();
+
+// app.use(cors({ origin: true }));
+const corsOptions = {
+    origin: process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 // config app
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoute(app);
 connectDB();
 
-
-let port =process.env.PORT || 6969;
+let port = process.env.PORT || 6969;
 // port == undefined = 6969
 
-app.listen(port,()=>{
-    console.log("node js is running on the port : "+port);
+app.listen(port, () => {
+  console.log("node js is running on the port : " + port);
 });
